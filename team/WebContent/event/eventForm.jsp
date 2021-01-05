dd95b706c9b22056645d7245f1565fa1f21e595c
kimsike <shinhye26@gmail.com>
2021-01-05 17:41:46 +0900

    servlet, eventForm 수정

--- team/WebContent/event/eventForm.jsp
+++ team/WebContent/event/eventForm.jsp
@@ -1,5 +1,12 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>    
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<c:set var="contextPath"  value="${pageContext.request.contextPath}"  />
+
+<%
+	request.setCharacterEncoding("UTF-8");
+%>
 <!DOCTYPE html>
 <html lang="zxx">
 <head>
@@ -59,7 +66,7 @@
     <section class="blog spad">
         <div class="container">
         	<h3>이벤트 등록</h3>
-            <form action="${contextPath}/event/addEvent.do" method="post" enctype="multipart/form-data" class="eventForm">	
+            <form action="${contextPath}/eventServlet/addEvent.do" method="post" enctype="multipart/form-data" class="eventForm">	
 				<input type="hidden" name="id" value="${id}"/>
 				<table class="table">
 					<colgroup>
--- team/src/event/EventController.java
+++ team/src/event/EventController.java
@@ -1,6 +1,8 @@
 package event;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -14,9 +16,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.fileupload.disk.DiskFileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FileUtils;
+
 @WebServlet("/eventServlet/*")
 public class EventController extends HttpServlet {
-
+	
+	String realPath;
 	EventService eventService; 
 	EventVO eventVO;
 	
@@ -36,7 +44,11 @@ public class EventController extends HttpServlet {
 		doHandle(request, response);
 	}
 	
-	protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		EventVO eventVO = new EventVO();
+		
+		realPath = request.getServletContext().getRealPath("/files/event");
 		
 		String nextPage = "";
 		request.setCharacterEncoding("UTF-8");
@@ -67,8 +79,39 @@ public class EventController extends HttpServlet {
 				nextPage = "/event/event.jsp";
 				
 			} else if (action.equals("/eventForm.do")) {
+				
 				nextPage = "/event/eventForm.jsp";
-			} else if (action.equals("/addEvent")) {
+				
+			} else if (action.equals("/addEvent.do")) {
+				
+				int eventNo = 0;
+				Map<String, String> eventMap = upload(request, response);
+				
+				String title = eventMap.get("eventTitle");
+				String content = eventMap.get("eventCotent");
+				String imageFileName = eventMap.get("eventImage");
+				
+				eventVO.setEventTitle(title);
+				eventVO.setEventContent(content);
+				eventVO.setEventImage(imageFileName);
+				
+				eventNo = eventService.addEvent(eventVO);
+				
+				if(imageFileName != null && imageFileName.length() != 0) {
+					
+					File srcFile = new File(realPath + "\\" + "temp" + "\\" + imageFileName);
+					File destDir = new File(realPath + "\\" + eventNo);
+					destDir.mkdir();
+					FileUtils.moveFileToDirectory(srcFile, destDir, true);
+		
+				}
+				
+				PrintWriter out = response.getWriter();
+				out.print("<script>");
+				out.print("window.alert('새글을 추가했습니다.')");
+				out.print("location.href='" + request.getContextPath() + "/eventServlet/event.do';");
+				out.print("</script>");
+				return;
 				
 			} else {
 				nextPage = "/event/event.jsp";
@@ -83,7 +126,57 @@ public class EventController extends HttpServlet {
 		
 	}
 	
-	
+	private Map<String, String> upload(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Map<String, String> eventMap = new HashMap<String, String>();
+		
+		String encoding = "utf-8";
+		File currentDirPath = new File(realPath);
+		DiskFileItemFactory factory = new DiskFileItemFactory();
+		factory.setSizeThreshold(1024*1024*1);
+		factory.setRepository(currentDirPath);
+		ServletFileUpload upload = new ServletFileUpload(factory);
+		
+		try {
+			List items = upload.parseRequest(request);
+			
+			for(int i=0; i<items.size(); i++){
+				
+				DiskFileItem fileItem = (DiskFileItem)items.get(i);
+				
+				if(fileItem.isFormField()) {
+					eventMap.put(fileItem.getFieldName(), fileItem.getString(encoding));
+					
+				} else {
+					
+					eventMap.put(fileItem.getFieldName(), fileItem.getName());
+					
+					if(fileItem.getSize() > 0) {
+						int idx = fileItem.getName().lastIndexOf("\\");
+						
+						if(idx == -1) {
+							idx = fileItem.getName().lastIndexOf("/");
+						}
+						
+						String fileName = fileItem.getName().substring(idx + 1);
+						eventMap.put(fileItem.getFieldName(), fileName);
+						String tempDirPath = realPath + "\\temp";
+						File tempDir = new File(tempDirPath);
+						
+						if(!tempDir.exists()) {
+							tempDir.mkdir();
+						}
+						File uploadFile = new File(currentDirPath + "\\temp\\" + fileName);
+						fileItem.write(uploadFile);
+					}
+				}
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return eventMap;
+	}
 	
 	
 }
